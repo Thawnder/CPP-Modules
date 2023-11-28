4a55c342dcd5c4da5d961ad468eaabad6aa768fa
Benjamin Pleutin <bpleutin@c2r8p2.42perpignan.fr>
2023-11-28 18:10:27 +0100

    fixes and start ex01

---
+++ CPP_00/ex01/phonebook.hpp
@@ -0,0 +1,22 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+class phonebook
+{
+private:
+	/* data */
+public:
+	phonebook(/* args */);
+	~phonebook();
+};
+
+phonebook::phonebook(/* args */)
+{
+}
+
+phonebook::~phonebook()
+{
+}
+
+
+#endif
